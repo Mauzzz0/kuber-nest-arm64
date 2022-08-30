3dc7a808c1f4376b97041f2b452e019c12863377
Ruslan Semak <ruslan.semack1501@gmail.com>
2022-08-31 01:31:46 +0300

    feat: New scripts, ESLint, package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,15 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -17,9 +20,11 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    '@typescript-eslint/semi': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'semi': [1, 'always']
   },
-};
+}
--- integration/start-services.sh
+++ integration/start-services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
-werf converge --repo mauzzz0/werf-first-app
+werf converge --repo mauzzz0/kuber-nest
