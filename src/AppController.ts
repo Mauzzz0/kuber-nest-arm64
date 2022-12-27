a4e6093d35a6fd68398fe8da04d73e281ee48e72
Ruslan Semak <ruslan.semack1501@gmail.com>
2022-12-27 21:58:13 +0300

    feat: Добавлена поддержка кеширования при помощи редиса. Добавлен docker-compose.yaml

--- src/AppController.ts
+++ src/AppController.ts
@@ -1,12 +1,52 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  CacheInterceptor,
+  CacheKey,
+  CacheTTL,
+  Controller,
+  Get,
+  Inject,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './AppService';
+import { Cache } from 'cache-manager';
+import { sleep } from './utils/sleep';
 
 @Controller()
 export class AppController {
-  constructor(private readonly service: AppService) {}
+  constructor(
+    private readonly service: AppService,
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
+  ) {}
 
   @Get('/ping')
   async ping() {
+    return this.service.ping();
+  }
+
+  @Get('/cache-manual')
+  async cacheManual() {
+    const key = 'key';
+    const cachedData = await this.cacheService.get<string>(key);
+
+    if (cachedData) {
+      return cachedData;
+    }
+
+    await sleep(2);
+    const result = this.service.ping();
+
+    await this.cacheService.set(key, result, { ttl: 10 });
+
+    return cachedData;
+  }
+
+  @CacheKey('custom-cache-key')
+  @CacheTTL(10)
+  @UseInterceptors(CacheInterceptor)
+  @Get('/cache-interceptor')
+  async cacheInterceptor() {
+    await sleep(2);
     return this.service.ping();
   }
 }
